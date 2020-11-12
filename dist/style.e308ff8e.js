38db1f9f79d20c83ba72eb88261872cb02196125
jmerinob <jmerinob@uoc.edu>
2020-11-12 19:46:43 +0100

    commit

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\UOC\\Herramientas HTML y CSS\\Actividades\\M2\\img\\clublector-isologo.png":[["clublector-isologo.6f4972b7.png","img/clublector-isologo.png"],"img/clublector-isologo.png"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\UOC\\Herramientas HTML y CSS\\Actividades\\M2\\img\\book-shelf.svg":[["book-shelf.a3d45e9b.svg","img/book-shelf.svg"],"img/book-shelf.svg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "22942" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38082" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
